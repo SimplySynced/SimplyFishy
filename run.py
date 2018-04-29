53c4e3962556cf72f46628fa7e1c008b72842df3
Rob Burgess <rob@simplysyncedllc.com>
2018-04-28 23:44:02 -0400

    Initial Commit

--- run.py
+++
@@ -1,9 +0,0 @@
-import simplyfishy
-import gpio_control
-from flask_socketio import SocketIO
-
-# SocketIO setup
-socketio = SocketIO(simplyfishy, async_mode='gevent')
-
-if __name__ == '__main__':
-    socketio.run(simplyfishy.simplyfishy, debug=True, host='0.0.0.0')
