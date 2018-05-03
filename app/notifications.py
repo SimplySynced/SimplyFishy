a96952bdd54fc85cf87149051a04c85c237a880f
Rob Burgess <rob@simplysyncedllc.com>
2018-05-03 12:45:51 -0400

    Fixed sorting issue with the outlets.

--- app/notifications.py
+++ app/notifications.py
@@ -1,12 +1,13 @@
 from pushbullet import Pushbullet
 from twilio.rest import Client
+from gevent import sleep
 
 # Set the pushbullet API key
-pb = Pushbullet('**')
+pb = Pushbullet('')
 
 # Set the Twilio API Authentication
-account_sid = "**"
-auth_token = "**"
+account_sid = ""
+auth_token = ""
 
 twil = Client(account_sid, auth_token)
 
@@ -14,13 +15,14 @@ def send(msg):
     # Send Pushbullet Note
     pb.push_note('Simply Fishy', msg)
 
-
+    sleep(5)
 
     # Send SMS via Twilio
     message = twil.messages.create(
-        to="+1111",
-        from_="+1111",
+        to="+1111111111",
+        from_="+1111111111",
         body=msg
     )
 
     print(message)
+
